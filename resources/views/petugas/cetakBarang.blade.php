8eb9a89d1552e3c95ad32d7754427a322a27622a
januarsetyo <januarsn2@gmail.com>
2022-08-10 22:19:32 +0700

    update

--- resources/views/petugas/cetakBarang.blade.php
+++ resources/views/petugas/cetakBarang.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- <meta name="csrf-token" content="{{ csrf_token }}"> --}}
+    <link rel="icon" href="../assets/img/surabaya.png">
     <style>
         table.static{
             position: relative;
